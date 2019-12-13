e44a06d3ac12c40af2ab9db34c26af5bfba869cc
myfreeer <myfreeer@users.noreply.github.com>
2019-12-13 21:34:54 +0800

    UnsafeAccessor: log exceptions with logger
    https://github.com/myfreeer/unsafe/issues/3

--- src/main/java/myfreeer/unsafe/utils/accessor/UnsafeAccessor.java
+++ src/main/java/myfreeer/unsafe/utils/accessor/UnsafeAccessor.java
@@ -6,15 +6,20 @@ import myfreeer.unsafe.utils.invoke.LookupFactory;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.AccessibleObject;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UnsafeAccessor implements Accessor {
+    private static final Logger log =
+            Logger.getLogger(UnsafeAccessor.class.getSimpleName());
+
     private final MethodHandle setAccessible;
 
     public UnsafeAccessor(final LookupFactory factory) {
         try {
             final MethodHandles.Lookup lookup = factory.lookup(AccessibleObject.class);
             setAccessible = lookup.findSetter(AccessibleObject.class,
-                        "override", boolean.class);
+                    "override", boolean.class);
         } catch (NoSuchFieldException | IllegalAccessException e) {
             throw new UnsafeException(e);
         }
@@ -25,11 +30,15 @@ public class UnsafeAccessor implements Accessor {
         try {
             setAccessible.invokeExact(ao, true);
             return true;
-        } catch (Throwable ignored) {
+        } catch (Throwable throwable) {
+            log.log(Level.INFO,
+                    "invokeExact on setAccessible fail, falling back",
+                    throwable);
             try {
                 ao.setAccessible(true);
                 return true;
-            } catch (Exception ignored1) {
+            } catch (Exception e) {
+                log.log(Level.WARNING, "setAccessible fail", throwable);
             }
         }
         return false;
