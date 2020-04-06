c8f20ede20853067cbfd5ebca5cebed3f9afaace
myfreeer <myfreeer@users.noreply.github.com>
2020-04-06 20:44:00 +0800

    UnsafeUtils: add java version parser form gson

--- src/main/java/myfreeer/unsafe/utils/UnsafeUtils.java
+++ src/main/java/myfreeer/unsafe/utils/UnsafeUtils.java
@@ -107,4 +107,90 @@ public class UnsafeUtils {
         }
         return accessor;
     }
+
+
+    private static final int majorJavaVersion = JavaVersion.determineMajorJavaVersion();
+
+    /**
+     * @return the major Java version, i.e. '8' for Java 1.8, '9' for Java 9 etc.
+     */
+    public static int getMajorJavaVersion() {
+        return majorJavaVersion;
+    }
+
+    /*
+     * Copyright (C) 2017 The Gson authors
+     *
+     * Licensed under the Apache License, Version 2.0 (the "License");
+     * you may not use this file except in compliance with the License.
+     * You may obtain a copy of the License at
+     *
+     * http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     * See the License for the specific language governing permissions and
+     * limitations under the License.
+     */
+
+    static class JavaVersion {
+
+        private static int determineMajorJavaVersion() {
+            String javaVersion = System.getProperty("java.version");
+            return getMajorJavaVersion(javaVersion);
+        }
+
+        static int getMajorJavaVersion(String javaVersion) {
+
+            // Visible for testing only
+            // Oracle defines naming conventions at
+            // http://www.oracle.com/technetwork/java/javase/versioning-naming-139433.html
+            // However, many alternate implementations differ.
+            // For example, Debian used 9-debian as the version string
+
+            int version = parseDotted(javaVersion);
+            if (version == -1) {
+                version = extractBeginningInt(javaVersion);
+            }
+            if (version == -1) {
+                return 7;  // Choose minimum supported JDK version as default
+            }
+            return version;
+        }
+
+        private static int parseDotted(String javaVersion) {
+            try {
+                // Parses both legacy 1.8 style and newer 9.0.4 style
+                String[] parts = javaVersion.split("[._]");
+                int firstVer = Integer.parseInt(parts[0]);
+                if (firstVer == 1 && parts.length > 1) {
+                    return Integer.parseInt(parts[1]);
+                } else {
+                    return firstVer;
+                }
+            } catch (NumberFormatException e) {
+                return -1;
+            }
+        }
+
+        private static int extractBeginningInt(String javaVersion) {
+            try {
+                StringBuilder num = new StringBuilder();
+                for (int i = 0; i < javaVersion.length(); ++i) {
+                    char c = javaVersion.charAt(i);
+                    if (Character.isDigit(c)) {
+                        num.append(c);
+                    } else {
+                        break;
+                    }
+                }
+                return Integer.parseInt(num.toString());
+            } catch (NumberFormatException e) {
+                return -1;
+            }
+        }
+
+    }
+
 }
---
+++ src/test/java/myfreeer/unsafe/utils/JavaVersionTest.java
@@ -0,0 +1,76 @@
+package myfreeer.unsafe.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/*
+ * Copyright (C) 2017 The Gson authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class JavaVersionTest {
+    @Test
+    public void testGetMajorJavaVersion() {
+        assertTrue(UnsafeUtils.getMajorJavaVersion() >= 7);
+    }
+
+
+    @Test
+    public void testJava7() {
+        // http://www.oracle.com/technetwork/java/javase/jdk7-naming-418744.html
+        assertEquals(7, UnsafeUtils.JavaVersion.getMajorJavaVersion("1.7.0"));
+    }
+
+    @Test
+    public void testJava8() {
+        assertEquals(8, UnsafeUtils.JavaVersion.getMajorJavaVersion("1.8"));
+        assertEquals(8, UnsafeUtils.JavaVersion.getMajorJavaVersion("1.8.0"));
+        assertEquals(8, UnsafeUtils.JavaVersion.getMajorJavaVersion("1.8.0_131"));
+        assertEquals(8, UnsafeUtils.JavaVersion.getMajorJavaVersion("1.8.0_60-ea"));
+        assertEquals(8,
+                UnsafeUtils.JavaVersion.getMajorJavaVersion("1.8.0_111-internal"));
+
+        // openjdk8 per https://github.com/AdoptOpenJDK/openjdk-build/issues/93
+        assertEquals(8, UnsafeUtils.JavaVersion.getMajorJavaVersion("1.8.0-internal"));
+        assertEquals(8,
+                UnsafeUtils.JavaVersion.getMajorJavaVersion("1.8.0_131-adoptopenjdk"));
+    }
+
+    @Test
+    public void testJava9() {
+        // Legacy style
+        // Oracle JDK 9
+        assertEquals(9, UnsafeUtils.JavaVersion.getMajorJavaVersion("9.0.4"));
+        // Debian as reported in https://github.com/google/gson/issues/1310
+        assertEquals(9, UnsafeUtils.JavaVersion.getMajorJavaVersion("9-Debian"));
+        // New style
+        assertEquals(9, UnsafeUtils.JavaVersion.getMajorJavaVersion("9-ea+19"));
+        assertEquals(9, UnsafeUtils.JavaVersion.getMajorJavaVersion("9+100"));
+        assertEquals(9, UnsafeUtils.JavaVersion.getMajorJavaVersion("9.0.1+20"));
+        assertEquals(9, UnsafeUtils.JavaVersion.getMajorJavaVersion("9.1.1+20"));
+    }
+
+    @Test
+    public void testJava10() {
+        // Oracle JDK 10.0.1
+        assertEquals(10, UnsafeUtils.JavaVersion.getMajorJavaVersion("10.0.1"));
+    }
+
+    @Test
+    public void testUnknownVersionFormat() {
+        // unknown format
+        assertEquals(7, UnsafeUtils.JavaVersion.getMajorJavaVersion("Java9"));
+    }
+}
